bddbe68c302abde5d4a88898ab6077db4d3cca37
“franabreu” <fraabrvaz@alum.us.es>
2020-08-10 12:33:38 +0200

    Added responsive design to location list and App view

--- src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
+++ src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const WeatherExtraInfo = ({ humidity, wind }) => (
     <div className="weatherExtraInfoCont">
-        <span className="extraInfoText">{`Humedad: ${humidity} % - `}</span>
+        <span className="extraInfoText">{`Humedad: ${humidity} %`}</span>
         <span className="extraInfoText">{`Viento: ${wind} km/h`}</span>
     </div>
 );
