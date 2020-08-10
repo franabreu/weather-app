1d8769588d6c1abd4b2ad4c9c94081abe0ff6fdb
“franabreu” <fraabrvaz@alum.us.es>
2020-08-10 19:35:08 +0200

    Changed api key to different location and added to .gitignore

---
+++ src/constants/api_url.js
@@ -0,0 +1,5 @@
+import { weather_api_key } from '../constants/api_keys';
+
+export const api_key = weather_api_key;
+export const url_base_weather = "https://api.openweathermap.org/data/2.5/weather";
+export const url_base_forecast = "https://api.openweathermap.org/data/2.5/forecast";
